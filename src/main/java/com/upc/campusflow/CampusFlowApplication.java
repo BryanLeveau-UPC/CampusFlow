e9a9d88d197acb93e16cba861a6d7f18ced4944d
Halosesparta <bryan.leveau13@gmail.com>
2025-04-28 18:09:23 -0500

    Initial commit

---
+++ src/main/java/com/upc/campusflow/CampusFlowApplication.java
@@ -0,0 +1,13 @@
+package com.upc.campusflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusFlowApplication.class, args);
+    }
+
+}
