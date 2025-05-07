4dcee688c288815b3cf252efa33079fefd7bc019
Mauro Solorzano Espinoza <108832953+MWSE24@users.noreply.github.com>
2025-05-07 16:32:17 -0500

    Add files via upload
    Modifique Controller, DTO, Model, Repository y Service de las tablas Recurso y Tarea

--- src/main/java/com/upc/campusflow/Model/Recurso.java
+++ src/main/java/com/upc/campusflow/Model/Recurso.java
@@ -1,4 +1,22 @@
 package com.upc.campusflow.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Recurso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRecurso;
+    private String tipoArchivo;
+    private String url;
+    private Date fechaSubida;
 }
--- src/main/java/com/upc/campusflow/Model/Tarea.java
+++ src/main/java/com/upc/campusflow/Model/Tarea.java
@@ -1,4 +1,24 @@
 package com.upc.campusflow.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTarea;
+    private String titulo;
+    private String descripcion;
+    private Date fechaLimite;
+    private String prioridad;
+    private String estado;
 }
