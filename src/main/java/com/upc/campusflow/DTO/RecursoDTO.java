fc2284ee1066654368e62c148b54f49a3a813539
Mauro Solorzano Espinoza <108832953+MWSE24@users.noreply.github.com>
2025-05-07 16:30:24 -0500

    Modifique RecursoDTO Y TareaDTO

--- src/main/java/com/upc/campusflow/DTO/RecursoDTO.java
+++ src/main/java/com/upc/campusflow/DTO/RecursoDTO.java
@@ -1,4 +1,16 @@
 package com.upc.campusflow.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecursoDTO {
+    private int idRecurso;
+    private String tipoArchivo;
+    private String url;
+    private Date fechaSubida;
 }
--- src/main/java/com/upc/campusflow/DTO/TareaDTO.java
+++ src/main/java/com/upc/campusflow/DTO/TareaDTO.java
@@ -1,4 +1,19 @@
 package com.upc.campusflow.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TareaDTO {
+    private int idTarea;
+    private String titulo;
+    private String descripcion;
+    private Date fechaLimite;
+    private String prioridad;
+    private String estado;
 }
